514af1f2e5a2df43a8611d1455dcf4acdd37a935
YourName <youremail@example.com>
2026-04-13 15:28:25 +0200

    getmedata

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,19 @@
-import usermodel from "../models/user.model.js"
+import expressAsyncHandler from "express-async-handler"
+import asyncHandler from "express-async-handler";
+import userModel from "../models/user.model";
 
 // Get My Profile
+export const getme = asyncHandler(async(req,res)=>{
+ const user = await User.findById(req.user._id).select("-password");
+
+
+  if (!user) {
+    res.status(404).json({message:"user not found"})
+  }
+   res.status(200).json({
+      success: true,
+      data: user,
+    });
+})
+
 
